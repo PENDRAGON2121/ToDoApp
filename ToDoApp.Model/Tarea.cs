a2671c88064e8f5e930fa7ea487b48ffd1b5ed1c
DavidG <carlosgranadosramirez@gmail.com>
2025-02-05 18:06:29 -0600

    Cambio

--- ToDoApp.Model/Tarea.cs
+++ ToDoApp.Model/Tarea.cs
@@ -8,7 +8,7 @@ namespace ToDoApp.Model
 {
     public class Tarea
     {
-        public string ID { get; set; }
+        public int ID { get; set; }
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Estado Estado { get; set; }
