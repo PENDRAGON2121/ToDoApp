caab83a0537d2a5ac96cfb75cb273a2271b2313a
Pendragon <66645373+PENDRAGON2121@users.noreply.github.com>
2025-02-12 21:00:41 -0600

    Actualización de dependencias y modelo de datos
    - Añadida referencia a `Microsoft.VisualStudio.Web.CodeGeneration.Design` versión `8.0.7` en `ToDoApp.csproj`.
    - Añadida nueva carpeta `Views\GestionDeTareas\` en `ToDoApp.csproj`.
    - Añadida anotación `[Key]` a la propiedad `ID` en `Tarea.cs`.
    - Propiedades `Titulo`, `Descripcion`, `Estado` y `Prioridad` ahora son nullable en `Tarea.cs`.

--- ToDoApp.Model/Tarea.cs
+++ ToDoApp.Model/Tarea.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ namespace ToDoApp.Model
 {
     public class Tarea
     {
+        // Propiedades de la clase
+        [Key]
         public int ID { get; set; }
-        public string Titulo { get; set; }
-        public string Descripcion { get; set; }
-        public Estado Estado { get; set; }
-        public Prioridad Prioridad { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public Estado? Estado { get; set; }
+        public Prioridad? Prioridad { get; set; }
     }
 }
